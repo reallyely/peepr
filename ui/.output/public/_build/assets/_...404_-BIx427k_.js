8c513aa3a2502fa8738c00e6a4706c388130bddb
Stephen A. Ely <sely1986@gmail.com>
2025-03-27 15:40:17 -0400

    chore: remove dist files from git history

--- ui/.output/public/_build/assets/_...404_-BIx427k_.js
+++
@@ -1,1 +0,0 @@
-import{i as t,t as e}from"./web-DrH_9yjJ.js";var a=e('<main class="w-full p-4 space-y-2"><h1 class="font-bold text-xl">Page Not Found');function o(){return t(a)}export{o as default};
